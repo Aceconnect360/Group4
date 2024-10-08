4fa31e081e88705d8d4cb796d7ee28cd3083c0e8
Aceconnect360 <aceconnectgroup@gmail.com>
2024-10-08 08:33:43 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const hamburger *document.querySelector(".hamburger");
-const navmenu *document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("ckick",()=>{
   hamburger.classlist.toggle("active");
